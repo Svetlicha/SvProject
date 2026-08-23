4c421d25bf7e6dc1f445f3de3e05ab075f280a45
Svetlicha <svetlichaa@gmail.com>
2026-08-23 23:55:17 +0300

    Add assets/supabase-config.js

---
+++ assets/supabase-config.js
@@ -0,0 +1,5 @@
+// This file contains only public browser configuration. Never add a service-role key here.
+window.SV_SUPABASE_CONFIG = Object.freeze({
+  url: 'https://fgrozpyyrdltlcmzmarx.supabase.co',
+  publishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZncm96cHl5cmRsdGxjbXptYXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1MDQ0NjEsImV4cCI6MjEwMzA4MDQ2MX0.PFqdb_5dxrU0KP34mXoev1zY1Qt7njLGIHLRcgAeMgA'
+});
